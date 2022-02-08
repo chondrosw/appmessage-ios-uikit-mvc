a2154163e5c64078a83c2a1d29962ba7f3b2ebc7
ChondroSW <chondrosatriowibowo@gmail.com>
2022-02-08 15:14:16 +0700

    Initial Commit

---
+++ AppMessage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppMessage
+//
+//  Created by Chondro Satrio Wibowo on 08/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
